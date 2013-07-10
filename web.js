985f16cd9b30580b5789c755d17d32b5d62fdfa4
chris <chris@ubuntu.(none)>
2013-07-10 10:57:07 -0500

    serving static files

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs'); 
+
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var output = fs.readFileSync('index.html');
+  response.send(output.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
